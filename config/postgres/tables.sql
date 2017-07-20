d20cef4e0c4933c820931deaf5ceaf17dbb90b28
thomaspiller <thomaspierre.piller@gmail.com>
2017-07-20 15:37:31 +0200

    adding a primary key constraint to each table

--- config/postgres/tables.sql
+++ config/postgres/tables.sql
@@ -8,7 +8,7 @@ mx_users : table of user
 DROP TABLE IF EXISTS mx_users ;
 
 create table mx_users (
-  id serial unique,
+  id serial,
   username citext unique,
   email citext unique,
   key text,
@@ -17,32 +17,34 @@ create table mx_users (
   date_validated timestamp,
   date_hidden timestamp,
   date_last_visit timestamp,
-  data jsonb
+  data jsonb,
+  CONSTRAINT mx_users_pkey PRIMARY KEY (id)
 );
 
 ALTER TABLE mx_users OWNER TO mapxw;
 
 create table if not exists mx_views (
-  id character varying(20) not null,
+  id character varying(20),
   country character varying(3),
   editor integer,
   target jsonb,
   date_modified timestamp with time zone,
   data jsonb,
-  type character varying(10)
+  type character varying(10),
+  CONSTRAINT mx_views_pkey PRIMARY KEY (id,date_modified)
 );
-
 
 ALTER TABLE mx_views OWNER TO mapxw;
 
 create table if not exists mx_sources (
-  id character varying(31) unique not null,
+  id character varying(31),
   country character varying(3),
   editor integer,
   target jsonb,
   date_modified timestamp with time zone,
   data jsonb,
-  type character varying(10)
+  type character varying(10),
+  CONSTRAINT mx_sources_pkey PRIMARY KEY (id)
 );
 
 ALTER TABLE mx_sources OWNER TO mapxw;
